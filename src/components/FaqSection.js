e90d1ebe63bcf72fa696bb70e6e63dd9d0212630
Mankge <mbongeni921@gmail.com>
2020-11-18 11:05:30 +0200

    FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAG</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I start</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Diffent Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do we offer.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
