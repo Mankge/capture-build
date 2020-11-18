6af8ea10c9b091a935b0eff84629a7f0faedb967
Mankge <mbongeni921@gmail.com>
2020-11-18 18:45:23 +0200

    Styling the FAQ Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,9 @@
+import styled from "styled-components";
+import { About } from "../styles";
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq className="faq">
       <h2>
         Any Questions <span>FAG</span>
       </h2>
@@ -10,6 +13,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet</p>
           <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -17,6 +21,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet</p>
           <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Diffent Payment Methods</h4>
@@ -24,6 +29,7 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet</p>
           <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What products do we offer.</h4>
@@ -31,9 +37,38 @@ const FaqSection = () => {
           <p>Lorem ipsum dolor sit amet</p>
           <p>Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet</p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
