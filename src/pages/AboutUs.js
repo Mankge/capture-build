a69436f949a2fa6c768b433add36a7f70d3ba3c8
Mankge <mbongeni921@gmail.com>
2020-11-18 10:56:57 +0200

    About Us and Services Sections

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+      <ServicesSection />
+    </div>
+  );
+};
+
+export default AboutUs;
